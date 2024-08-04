ea90d5bf7b261643af635db7fb49d23bf9d4865a
wormmeatapple <127395841+wormmeatapple@users.noreply.github.com>
2024-08-04 14:19:34 +0800

    Update wikispeedrun.py
    i am v stupid and just cleaned up mistakes :D

--- wikispeedrun.py
+++ wikispeedrun.py
@@ -3,15 +3,22 @@ import re
 
 
 def get_links(link):
-    pattern = r'(/wiki/[^ "]+)'
+    links = []
+    linkpattern = r'(/wiki/[^ "]+)'
+    cleanpattern = r'/wiki/()'
     url = f'https://en.wikipedia.org/wiki/{link}'
     response = requests.get(url)
     text = response.text
-    links = re.findall(pattern, text)
-
+    dirtylinks = re.findall(linkpattern, text)
+    while "/wiki/Main_Page" in dirtylinks:
+            dirtylinks.remove("/wiki/Main_Page")
+    for l in dirtylinks:
+        if ":" not in l:
+            links.append(l)
+            
     return links
 
-for match in get_links('Shed'):
+for match in get_links('Orthogonal_defect_classification'):
     print(match)
 
 

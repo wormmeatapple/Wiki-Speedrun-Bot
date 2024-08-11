896ba7d14cda66bfddfb0ee4a3f4da746353e790
wormmeatapple <127395841+wormmeatapple@users.noreply.github.com>
2024-08-11 16:18:51 +0800

    Update wikispeedrun.py
    had to learn tuples for this :cry:

--- wikispeedrun.py
+++ wikispeedrun.py
@@ -1,11 +1,14 @@
 import requests
 import re
+from collections import  deque
 
+
+start = input("What's the starting point: ")
+target = input("What's the target: ")
 
 def get_links(link):
     links = []
     linkpattern = r'(/wiki/[^ "]+)'
-    cleanpattern = r'/wiki/()'
     url = f'https://en.wikipedia.org/wiki/{link}'
     response = requests.get(url)
     text = response.text
@@ -13,12 +16,39 @@ def get_links(link):
     while "/wiki/Main_Page" in dirtylinks:
             dirtylinks.remove("/wiki/Main_Page")
     for l in dirtylinks:
-        if ":" not in l:
+        if ":" not in l and l not in links:
             links.append(l)
             
-    return links
+    return set(links)
 
-for match in get_links('Orthogonal_defect_classification'):
-    print(match)
+def wiki_speedrun(start, target):
+    visited = set()
+    queue = deque([(start, [start])])
+
+    while queue:
+        current_page, path = queue.popleft()
+
+        if current_page == target:
+            return path
+        
+        if current_page in visited:
+            continue
+        
+        visited.add(current_page)
+
+        links = get_links(current_page)
+
+        for link in links:
+            queue.append((link, path + [link]))
+            print(f"Added", link, "to queue!")
+
+path = wiki_speedrun(start, target)
+print(path)
+
+
+
+
+
+
 
 
